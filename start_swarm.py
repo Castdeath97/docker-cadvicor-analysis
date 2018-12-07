0b82fff41170b83b7d7bf318dcccd171f569f193
Ammar Hasan <ammar199733@gmail.com>
2018-12-07 15:17:53 +0000

    Docker swarm in task two can now be created using python SDK

--- start_swarm.py
+++ start_swarm.py
@@ -8,17 +8,43 @@ PRIME_IMAGE = "nclcloudcomputing/javabenchmarkapp"
 VISUAL_IMAGE = "dockersamples/visualizer"
 MONGO_IMAGE = "mongo"
 
-# Replicas
-BENCH_REP = 2
+# Replicas for prime web
+PRIME_REP = 2
 
-client = docker.from_env() # docker client
-client.swarm.init() # start swarm
-net = client.networks.create("network", scope = "swarm") # create network
+# docker client
+client = docker.from_env() 
+ # start swarm
+client.swarm.init()
+# create network
+net = client.networks.create(name = "webnet", scope = "swarm", driver = "overlay") 
 
-# Services
+# Services (all in the same swarm network (networks = [net.id]))
 
-client.services.create(image = PRIME_IMAGE, networks = net.id).scale(BENCH_REP)
-client.services.create(image = VISUAL_IMAGE, networks = net.id, constraints = "node.role == manager")
-client.services.create(image = MONGO_IMAGE, networks = net.id)
+# prime webserver (port 8080<>8080)
+client.services.create(
+    name = "prime",
+    image = PRIME_IMAGE, 
+    networks = [net.id],
+    endpoint_spec = docker.types.EndpointSpec(ports = {8080:8080})
+    ).scale(PRIME_REP) # scale for replicas
 
-# vol, , port
+# docker visualiser webserver (port 3000<>8080)
+# set up required vols and constraints specfied in its github
+client.services.create(
+    name = "visualizer",
+    image = VISUAL_IMAGE, networks = [net.id], 
+    constraints = ["node.role == manager"],
+    mounts = [ "/:/rootfs:ro","/var/run:/var/run:ro","/sys:/sys:ro"
+    ,"/var/lib/docker/:/var/lib/docker:ro","/dev/disk/:/dev/disk:ro"],
+    endpoint_spec = docker.types.EndpointSpec(ports = {4000:8080})
+    )
+
+# mongo database (port 3306<>27017)
+# set up vol for presistence 
+client.services.create(
+    name = "mongo",
+    image = MONGO_IMAGE,
+    networks = [net.id],
+    mounts = ["/data/db"],
+    endpoint_spec = docker.types.EndpointSpec(ports = {3306:27017})
+    )
